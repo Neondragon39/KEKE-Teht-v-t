8dd9ea56d767a3b9072e4595c99c4285f08b3582
Neondragon39 <97505790+Neondragon39@users.noreply.github.com>
2022-02-14 14:54:07 +0200

    Add files via upload

---
+++ Tehtävä_4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tehtävä_3_pituusmuunnin_Vladislav_jeskin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Otsikko
+            Console.WriteLine("Valuuttalaskuri");
+            Console.WriteLine("----------------");
+            Console.WriteLine("");
+
+            Console.Write("Anna summa euroina");
+            Euro = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            //Laskee eurot dollareiksi ja puntiksi
+            string dol = string.Format("{0:0.00}", Euro*1.13);
+            string pun = string.Format("{0:0.00}", Euro * 0.84);
+            Console.WriteLine($"{dol} $");
+            Console.WriteLine($"{pun} £");
+        }
+    }
+}
