1ec05d88b3dbd34980d201145d6b215f9647170c
Neondragon39 <97505790+Neondragon39@users.noreply.github.com>
2022-03-06 22:50:19 +0200

    Add files via upload

---
+++ Tehtävä 7.1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Tehtävä_7._1_Lukujen_tulostus_Vladislav_Jeskin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Luku;
+            Console.WriteLine("Tehtävä 7.1 ");
+            Console.WriteLine("");
+
+            Console.WriteLine("-------------------");
+
+            Console.Write("Anna aloituskuku? ");
+            Luku = int.Parse(Console.ReadLine());
+            for (int i = Luku; i >= 0; i-- )
+            {
+                Console.WriteLine($"{i}");
+            }
+
+        }
+    }
+}
