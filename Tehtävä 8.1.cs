0637ff487394331bd18e6bb78672ce9ab4efb6db
Neondragon39 <97505790+Neondragon39@users.noreply.github.com>
2022-03-07 10:11:46 +0200

    Add files via upload

---
+++ Tehtävä 8.1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Tehtävä_8._1_Hedelmät_Vladislav_jeskin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,x;
+            Console.WriteLine("Tehtävä 8.1! ");
+            Console.WriteLine("");
+
+            Console.WriteLine("------------------");
+            Console.WriteLine("");
+
+            Console.Write("Montako hedelmää halut nähdä ?");
+            //number of fruits!
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            string[] nimet = { "Omena", "Banaani","Luumu" };
+
+            Console.WriteLine("");
+
+            for (i=0;i<x;i++)
+            {
+                Console.WriteLine($"{nimet[i]}");
+            }
+        }
+    }
+}
