91c7da374974b19339ac42d20e706f7c298cacf9
Neondragon39 <97505790+Neondragon39@users.noreply.github.com>
2022-03-18 11:46:19 +0200

    Add files via upload

---
+++ Tehtävä 10.1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Tehtävä_10._1_Lukujen_summa_Vladislav_jeskin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tehtävä 10.1! ");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("");
+            int Luku = 0 ;
+            int x = 0;
+            int Vastaus=0;
+
+            Console.WriteLine("Anna kokonaislukuja, niin lasken summan reaaliajassa ");
+            while ( Vastaus < 100)
+            {
+
+                x++;
+                Console.Write("Anna "+ (x) + ". luku? ");
+                Luku  = int.Parse(Console.ReadLine());
+                Vastaus += Luku;
+                Console.WriteLine("Summa on nyt "+ Vastaus);
+
+            }
+            Console.WriteLine("Raja on saavutettu!!!!");
+        }
+    }
+}
