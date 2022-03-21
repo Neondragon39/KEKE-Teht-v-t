c93b716265f55a56698db46ee604e326e6f5b7d7
Neondragon39 <97505790+Neondragon39@users.noreply.github.com>
2022-03-21 13:32:42 +0200

    Add files via upload

---
+++ tehtävä 12.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Tehtävä_12_Aliohjelma_1_Vladislav_Jeskin
+{
+    class Program
+    {
+        public class Neliöitä
+        {
+
+
+            public static int NelionAla(int luku)// aliohjelma
+            {
+                
+                int tulos = luku * luku;// kanta * korkeus
+                return tulos;
+            }
+
+            public static void Main()//main ohjelma
+            {
+
+                Console.WriteLine("Tehtävä 12.1!");
+                Console.WriteLine("-------------------");
+                Console.WriteLine("");
+                // tallennetut lyhenteet
+                int luku;
+                int ala;
+                Console.Write("Anna luku, jonka neliön haluat tietää ");
+                luku = int.Parse(Console.ReadLine());//käyttäjän tuoma neliön pinta alan alku
+                
+                
+                    
+                 
+                 ala = NelionAla(luku);// aliohjelma laskee
+
+                Console.WriteLine("Ala on {0:0.00}", ala);// tulostus
+
+
+            }
+
+        }
+    }
+}
+
