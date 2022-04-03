929d31e0cef37ab9fe6a4a55b46ee566cd213e52
Neondragon39 <97505790+Neondragon39@users.noreply.github.com>
2022-04-03 23:08:55 +0300

    Add files via upload

---
+++ Tehtävä 14.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Tehtävä_14_syötteen_tarkistus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tehtävä 14 ");
+            Console.WriteLine("-------------------");
+            Console.WriteLine("");
+
+            int num = 0;
+
+            Console.WriteLine("Anna kokonaisluku? ");
+            string numInput = Console.ReadLine();
+
+            bool success = Int32.TryParse(numInput, out num);
+
+            if (success)
+            {
+                Console.WriteLine("convert '"+numInput+"' to "+numInput+". NIICE!!" );
+            }
+            else 
+            {
+                Console.WriteLine($"Attempted conversion of '{numInput}' failed. SORRY! ");
+            }
+
+        }
+    }
+}
