1476b922ec49d11faacdf30dabb2167536dd47d4
Neondragon39 <97505790+Neondragon39@users.noreply.github.com>
2022-11-09 10:58:18 +0200

    Add files via upload

--- Olio1.cs
+++ Olio1.cs
@@ -1,85 +1,59 @@
-amespace Olio1
-{
-    class Kirjasto
-{
-    //kentät
+﻿class Kirjasto//kirjastoluokka
+{   //kenttä ja ovat yksityiset
+    private string otsikko;
+    private string kirjailija;
+    private int sivumaara;
+
+
+
     private int erapaiva;
-    private int sivumäärä;
+    private int tyyppi;
     private string nimi;
-    //= määrittelemäsi tyypit(mm.kirjan nimi, sivumäärä, kirjailija jne.)
 
-    //metodit
-    public int Erapaiva()
-    {
-        Console.WriteLine("Erapaiva metodia käytetty");
-        return erapaiva;
-    }
-    public int Sivumäärä()
-    {
-        Console.WriteLine("Sivumäärä metodia käytetty");
-        return sivumäärä;
-    }
-    public string Nimi()
-    {
-        Console.WriteLine("Nimi metodia käytetty");
-        return nimi;
-    }
-    public string PalautaTeoksenNimi()
-    {
-        Console.WriteLine("PalautaTeoksenNimi metodia käytetty");
 
-        return nimi;
-    }
-    //oletuskonstruktio
-    public Kirjasto()
-    {
-        erapaiva = 0; //oletetaan, että tässä on lainausaika, esim. 30 päivää.
-                      //ei koko päiväystä, joka vaatisi useamman eri muuttujan
-        sivumäärä = 0;
-        nimi = ""; //kaksi lainausmerkkiä alustaa merkkijonon arvoksi null
 
-    }
-    //Ylikuormitettu konstruktori
-    public Kirjasto(int u_erapaiva, int u_sivumäärä, string u_nimi)
-    {
-        erapaiva = u_erapaiva;
-        tyyppi = u_sivumäärä;
-        nimi = u_nimi;
-
+    //metodi jotka muutettu julkiseksi
+    public string Otsikko
+    { //otsikko pitää olla isolla kirjaimella muuten ei toimi
+        get { return otsikko; }//päästäkseen käsiksi yksityiskenttään
+        set { otsikko = value; }//sitten kohteeseen, jonka olen asettanut tallentamaan yksityiseen kenttään.                         
     }
 
+    public string Kirjailija
+    {
+        get { return kirjailija; }
+        set { kirjailija = value; }
+    }
+
+    public int Sivumaara//tämä ei anna muuttaa sivumäärää kirja1 sestä program puolella.
+    {
+        get { return sivumaara; }
+        set
+        {
+            if (value >= 200)
+            {
+                sivumaara = 200;
+            }
+        }
+    }
 }
-class Olio1
+class Program
 {
     static void Main(string[] args)
     {
-        Kirjasto tallenne;
-        tallenne = new Kirjasto();
-        int uusi_erapaiva = tallenne.Erapaiva();
-        int tyyppi = tallenne.Sivumäärä();
-        string nimi = tallenne.Nimi();
-        double u_sivumäärä, u_nimi, u_erapaiva;
-        try
-        {
-            Console.WriteLine("Anna kirjan Sivunmäärä: ");
-            u_sivumäärä = double.Parse(Console.ReadLine());
-            Console.WriteLine("Anna kirjan erapaiva: ");
-            u_erapaiva = double.Parse(Console.ReadLine());
-            Console.WriteLine("Anna kirjan nimi: ");
-            u_nimi = double.Parse(Console.ReadLine());
-
-            Kirjasto kirjasto = new Kirjasto();
-            kirjasto.Erapaiva = u_erapaiva;
-            kirjasto.Sivumäärä = u_sivumäärä;
+        Kirjasto Kirja = new Kirjasto(); // 
+        Kirja.Kirjailija = "Arthur Snowger";
+        Kirja.Otsikko = "Benjamin";
+        Kirja.Sivumaara = 750;
+           
+        Kirjasto uusi_erapaiva = new Kirjasto();
+        Kirjasto uusi_tyyppi = new Kirjasto();
+        Kirjasto uusi_nimi = new Kirjasto();
 
 
-        }
-        catch
-        {
-            Console.WriteLine("palauta");
-        }
-        Console.Writeline("\n\npaina jotain näppäitä jatkaaksesi...");
-        Console.ReadKey(true);
+
+        Console.WriteLine(+Kirja.Sivumaara + " Kirjailijan " + Kirja.Kirjailija);
+
+        Console.ReadLine();
     }
 }
-  }
